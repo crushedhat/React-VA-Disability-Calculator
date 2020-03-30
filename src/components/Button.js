d362721f0fa81a242438968e63d875009c73936e
Joe Mora <morajp3@gmail.com>
2020-03-30 10:00:43 -0400

    refenced id an onClick now outputs the id value

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import './Button.css';
 class Button extends Component {
     render() {
         return (
-            <div className='button' onClick={() => this.props.handleClick(this.props.children)}>
+            <div id={this.props.id} className='button' onClick={() => this.props.handleClick(this.props.id)}>
                 {this.props.children}
             </div>
         );
