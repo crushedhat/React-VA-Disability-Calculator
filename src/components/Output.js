533128db5ada848f4085b0ddb09902fe26c21e53
Joe Mora <morajp3@gmail.com>
2020-04-01 09:04:07 -0400

    added removerPercentage() and function call onClick

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Output.css';
 
+const removePercentage = () => {
+  console.log("here");
+}
+
 const List = props => (
   <div className="percentage-container">
       {
         props.items.map((item, index) => <div className="percentage" key={index}>
         <div className="percentage-text">{item}%</div>
-        <button className="exit"><span>X</span></button>
+        <button className="exit" onClick={removePercentage}><span>X</span></button>
         </div>)
       },
   </div>
