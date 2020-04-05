0b0a1c55c2065a46a3a5dde6bf30d6d9c9f5bd31
Joe Mora <morajp3@gmail.com>
2020-04-04 22:06:01 -0400

    item passed into Percent Component to create List item

--- src/components/Percent.js
+++ src/components/Percent.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 
 class Percent extends Component {
     render(){
-     
+        const {title} = this.props;
         return (
-          
+            <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+                <h6>{title}</h6>
+            </li>
         )
     }
 }
