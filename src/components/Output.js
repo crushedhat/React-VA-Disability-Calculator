d922fc06bd2602b6fc778bc6970e0fc646028fc0
Joe Mora <morajp3@gmail.com>
2020-04-15 09:23:53 -0400

    mergered Percent.js with Output.js; Percent.js was redundant

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import Percent from './Percent';
+//import Percent from './Percent';
 import uuid from 'react-uuid';
 /* import './Output.css'; */
 
 class List extends Component {
   render() {
-    const {items} = this.props
+    const {items, deleteItem} = this.props
     return (
       <ul className="list-group my-5">
           <h3 className="text-capitalize text-center"></h3>
             {
+      
               items.map(item => { 
                 return(
-                    <Percent key={uuid()} title={item.title} />
+                  <li className="list-group-item text-capitalize my-2" key={uuid()}>
+                    <h6>{item.percent}%</h6>
+                    <button className="deleteItem" onClick={deleteItem}>Delete</button>
+                  </li>
                 )
               })
             }
